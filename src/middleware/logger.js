3a02fc5ed5622fc93be6aee85246737de7e14aa6
Oleksandr Pavlenko <aleksandr.pavlenko2016@gmail.com>
2026-09-07 13:23:06 +0300

    use logger middleware

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -1,12 +1,9 @@
-import pinoHttp from 'pino-http';
-import pino from 'pino';
+// import pinoHttp from 'pino-http';
+// import pino from 'pino';
 
-const loggerMiddleware = pinoHttp({
-  logger: pino({
-    transport: {
-      target: 'pino-pretty',
-    },
-  }),
-});
+export const logger = (req, res, next) => {
+  console.log(`[LOG] ${req.method} ${req.url}`);
+  next(); // <-- Обязательно вызываем функцию next!
+};
 
-export default loggerMiddleware;
+export default logger;
